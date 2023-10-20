0f91e5365576d092ab89a254ec01b62a649aa4e7
Luanagithub <122471005+Luanagithub@users.noreply.github.com>
2023-10-20 10:30:26 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     rollupOptions: {
       external: ['styled-components'],
       external: ['react-router-dom'],
+      external: ['react-icons'],
     },
   },
 });
